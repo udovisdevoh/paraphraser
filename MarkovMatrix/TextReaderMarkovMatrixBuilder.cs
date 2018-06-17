1cdb1ae7ea071cac44b70d89f7495c4ea128ca71
Guillaume Lacasse <glacasse@gmail.com>
2018-06-17 10:43:43 -0400

    MarkovMatrixBuilder

---
+++ MarkovMatrix/TextReaderMarkovMatrixBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovMatrices
+{
+    public class TextReaderMarkovMatrixBuilder : IMarkovMatrixBuilder
+    {
+        public IMarkovMatrix BuildMatrix()
+        {
+            #warning Implement
+            throw new NotImplementedException();
+        }
+    }
+}
