bae22269b9d6bddf42a4597764da7b9c97110cd1
Guillaume Lacasse <glacasse@gmail.com>
2018-06-20 20:37:13 -0400

    SpellChecking work in progress

---
+++ SpellChecking/SpellChecker.cs
@@ -0,0 +1,50 @@
+﻿using NHunspell;
+using StringManipulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellChecking
+{
+    public class SpellChecker : ISpellChecker
+    {
+        #region Members
+        private Language language;
+
+        private Hunspell hunspell;
+        #endregion
+
+        #region Constructors
+        public SpellChecker(Language language)
+        {
+            this.language = language;
+
+            if (this.language == Language.English)
+            {
+                this.hunspell = new Hunspell("en.aff", "en.dic");
+            }
+            else if (this.language == Language.French)
+            {
+                this.hunspell = new Hunspell("fr.aff", "fr.dic");
+            }
+        }
+        #endregion
+
+        public string GetCorrectedText(string originalText)
+        {
+            #warning Implement
+            #warning Add unit tests
+
+            string[] words = WordExtractor.GetWords(originalText);
+
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            this.hunspell.Dispose();
+        }
+    }
+}
---
+++ SpellChecking/SpellCheckingInterfaces/ISpellChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellChecking
+{
+    public interface ISpellChecker : IDisposable
+    {
+        string GetCorrectedText(string originalText);
+    }
+}
