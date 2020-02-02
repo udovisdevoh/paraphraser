768dd6ecccb08d1315d22307ecf87086622bf512
Guillaume Lacasse <glacasse@gmail.com>
2020-02-02 14:09:17 -0500

    Works pretty good (must fix "Would be detected as"), and show real detected language, add fuzzy logic

--- LanguageDetectorApp/MainWindow.Designer.cs
+++ LanguageDetectorApp/MainWindow.Designer.cs
@@ -74,6 +74,7 @@
             // 
             this.richTextBoxPathFindingOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxPathFindingOutput.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBoxPathFindingOutput.Location = new System.Drawing.Point(12, 235);
             this.richTextBoxPathFindingOutput.Name = "richTextBoxPathFindingOutput";
             this.richTextBoxPathFindingOutput.ReadOnly = true;
--- LanguageDetectorApp/PathFindingBackgroundWorker.cs
+++ LanguageDetectorApp/PathFindingBackgroundWorker.cs
@@ -129,7 +129,7 @@ namespace LanguageDetectorApp
 
             this.textBox.BeginInvoke((Action)(() =>
             {
-                this.richTextBoxPathFindingOutput.Text = detectedLanguage + ":\r\n" + pathFindingText + "\r\n" + otherMatchLanguage + ":\r\n" + lastNodeText;
+                this.richTextBoxPathFindingOutput.Text = detectedLanguage + ":\r\n" + pathFindingText + "\r\nWould be detected as " + otherMatchLanguage + ":\r\n" + lastNodeText;
                 foreach (int[] currentColorPositions in coloredPositions)
                 {
                     foreach (int currentColorPosition in currentColorPositions)
